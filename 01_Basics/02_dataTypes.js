48dc239feff0256334eaa0e7436fe881584d921d
Pratham Shrivastava <145213249+Paskert@users.noreply.github.com>
2024-07-28 20:55:32 +0530

    Create 02_dataTypes.js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "pratham" // string
+let age = 21 // number
+let isLoggedIn = false //boolean
+// number 
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+console.log(typeof null)
