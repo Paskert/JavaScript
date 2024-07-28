033ac1e6ffb8ccf41061bdac5de9545163422ad1
Pratham Shrivastava <prathamshrivastava.ps@gmail.com>
2024-07-29 00:50:17 +0530

    edits to operations

--- 01_Basics/03_conversionOperstion.js
+++ 01_Basics/03_conversionOperstion.js
@@ -13,3 +13,34 @@ console.log(valueInNumber)
 let isBool = "abcd"
 let boolbool = Boolean(isBool)
 console.log(boolbool) // true
+
+// --------------------- Operations --------------------------
+let value = 3
+let negval = -value // -3 duh
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+
+let str1 = "Hello "
+let str2 = "Pratham"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1 + 2 + "2")
+
+console.log(+true) // 1
+console.log(+"") // 0
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1++)
+console.log (++num2)
+
+// prefix and postfix opperators
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
