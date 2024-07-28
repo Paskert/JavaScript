ad3cd7488e041109a4a9c0d401761a4c0842d26c
Pratham Shrivastava <prathamshrivastava.ps@gmail.com>
2024-07-28 23:55:48 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperstion.js
@@ -0,0 +1,15 @@
+let score = "three"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+//  "three" => NaN
+
+let isBool = "abcd"
+let boolbool = Boolean(isBool)
+console.log(boolbool) // true
