0c436eb1428ff0d926e59cafd2cf35f83f38063d
Paskert <prathamshrivastava.ps@gmail.com>
2025-06-14 07:23:00 +0530

    rename

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -7,8 +7,10 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
 
+// result from conversion of
 // "33" => 33
 //  "three" => NaN
+// true => 1 ; false => 0
 
 let isBool = "abcd"
 let boolbool = Boolean(isBool)
@@ -27,12 +29,13 @@ let negval = -value // -3 duh
 let str1 = "Hello "
 let str2 = "Pratham"
 
-// let str3 = str1 + str2
-// console.log(str3)
+let str3 = str1 + str2
+console.log(str3)
 
 // console.log("1" + 2)
 // console.log(1 + "2")
 // console.log("1" + "2")
+// console.log("1" + 2 + 2)
 // console.log(1 + 2 + "2")
 
 console.log(+true) // 1
