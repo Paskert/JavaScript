2af8811dbcbafad1739c8a9edea21e8bb69a73a6
Paskert <prathamshrivastava.ps@gmail.com>
2025-06-15 16:49:24 +0530

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -3,24 +3,27 @@ const age = 21
 
 // console.log(name + age + " Hello")  // Don't do this, Not recommended
 
-console.log(`Hello My name is ${name} and my age is ${age}`)
+//This is called string interpolation
+console.log(`Hello My name is ${name} and my age is ${age}`) 
 
-const gameName = new String('Sifu-game') //this string will be an object(its element will be strored like key value pair not like array)
+//this string will be an object(its element will be strored like key value pair not like array)
+const gameName = new String('Sifu-game') 
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase)
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('i'))
+console.log(gameName[0]) //S
+console.log(gameName.length) //9
+console.log(gameName.toUpperCase()) // SIFU-GAME
+console.log(gameName.charAt(2)) //f
+console.log(gameName.indexOf('i')) //1
 
 const newString = gameName.substring(0,4)
-console.log(newString)
+console.log(newString) //Sifu
 
 const newStringtwo = gameName.substring(-2,4)
-console.log(newStringtwo)
-console.log(newStringtwo.trim())
+console.log(newStringtwo) //Sifu
+console.log(newStringtwo.trim()) //Sifu
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20','-'))
-console.log(url.includes('sundar'))
-console.log(gameName.split('-'))
+console.log(url.replace('%20','-')) //https://hitesh.com/hitesh-choudhary
+console.log(url.includes('sundar')) // false
+console.log(gameName.split('-'))  // [ 'Sifu', 'game' ]
